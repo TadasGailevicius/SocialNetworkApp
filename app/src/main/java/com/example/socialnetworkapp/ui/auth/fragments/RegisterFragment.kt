cf3b0bd6fc820130756135b2d187f40b7a8a4248
Tadas <tadasgailevicius@yahoo.com>
2020-11-19 00:44:18 +0200

    Add LoginFragment.kt class Add RegisterFragment.kt class

---
+++ app/src/main/java/com/example/socialnetworkapp/ui/auth/fragments/RegisterFragment.kt
@@ -0,0 +1,8 @@
+package com.example.socialnetworkapp.ui.auth.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.socialnetworkapp.R
+
+class RegisterFragment : Fragment(R.layout.fragment_register){
+
+}
