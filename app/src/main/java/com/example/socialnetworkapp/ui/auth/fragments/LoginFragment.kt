7c5037485024d18f3ced627c9c273746f81bd41e
Tadas <tadasgailevicius@yahoo.com>
2020-11-19 23:40:18 +0200

    Add method to LoginFragment.kt class

--- app/src/main/java/com/example/socialnetworkapp/ui/auth/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/socialnetworkapp/ui/auth/fragments/LoginFragment.kt
@@ -1,8 +1,22 @@
 package com.example.socialnetworkapp.ui.auth.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.socialnetworkapp.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment(R.layout.fragment_login){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvRegisterNewAccount.setOnClickListener {
+            findNavController().navigate(
+                    LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+            )
+        }
+    }
 
 }
