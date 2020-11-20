b519cf2ac968dc0b0c66622b817380cdc17014a2
Tadas <tadasgailevicius@yahoo.com>
2020-11-21 00:53:10 +0200

    Add lines to DefaultAuthRepository.kt class

--- app/src/main/java/com/example/socialnetworkapp/repositories/DefaultAuthRepository.kt
+++ app/src/main/java/com/example/socialnetworkapp/repositories/DefaultAuthRepository.kt
@@ -1,9 +1,20 @@
 package com.example.socialnetworkapp.repositories
 
+import com.example.socialnetworkapp.data.entities.User
 import com.example.socialnetworkapp.other.Resource
+import com.example.socialnetworkapp.other.safeCall
 import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class DefaultAuthRepository : AuthRepository {
+
+    val auth = FirebaseAuth.getInstance()
+    val users = FirebaseFirestore.getInstance().collection("users")
 
     override suspend fun login(email: String, password: String): Resource<AuthResult> {
         TODO("Not yet implemented")
@@ -14,7 +25,16 @@ class DefaultAuthRepository : AuthRepository {
         username: String,
         password: String
     ): Resource<AuthResult> {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO){
+            safeCall {
+                val result = auth.createUserWithEmailAndPassword(email, password).await()
+                val uid = result.user?.uid!!
+                val user = User(uid, username)
+                users.document(uid).set(user).await()
+                Resource.Success(result)
+
+            }
+        }
     }
 
 }
