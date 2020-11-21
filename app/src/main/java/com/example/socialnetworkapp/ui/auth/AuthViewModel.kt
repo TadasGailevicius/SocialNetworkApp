11948ab09a4a33c65ce6562ba3d90a48b89ad484
Tadas <tadasgailevicius@yahoo.com>
2020-11-22 00:01:53 +0200

    Add lines to AuthViewModel.kt class

--- app/src/main/java/com/example/socialnetworkapp/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/socialnetworkapp/ui/auth/AuthViewModel.kt
@@ -1,6 +1,17 @@
 package com.example.socialnetworkapp.ui.auth
 
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ServiceLifecycleDispatcher
 import androidx.lifecycle.ViewModel
+import com.example.socialnetworkapp.repositories.AuthRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 
-class AuthViewModel : ViewModel() {
+class AuthViewModel @ViewModelInject constructor(
+        private val repository: AuthRepository,
+        private val applicationContext: ApplicationContext,
+        private val dispatcher: CoroutineDispatcher = Dispatchers.Main
+
+) : ViewModel() {
 }
