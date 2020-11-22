c7b5c03ef476a25f3bd145de55fc7f827e439481
Tadas <tadasgailevicius@yahoo.com>
2020-11-22 21:48:31 +0200

    Fix SettingsFragment.kt fragment

--- app/src/main/java/com/example/socialnetworkapp/ui/main/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/socialnetworkapp/ui/main/fragments/SettingsFragment.kt
@@ -3,6 +3,6 @@ package com.example.socialnetworkapp.ui.main.fragments
 import androidx.fragment.app.Fragment
 import com.example.socialnetworkapp.R
 
-class SettingsFragment : Fragment(R.layout.fragment_create_post){
+class SettingsFragment : Fragment(R.layout.fragment_settings){
 
 }
