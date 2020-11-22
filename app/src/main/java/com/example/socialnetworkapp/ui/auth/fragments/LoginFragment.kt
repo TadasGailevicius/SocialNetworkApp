888a257396bd09fab298a4f77c015b0c4ef0f8bc
Tadas <tadasgailevicius@yahoo.com>
2020-11-22 20:43:35 +0200

    Add lines to LoginFragment.kt fragment

--- app/src/main/java/com/example/socialnetworkapp/ui/auth/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/socialnetworkapp/ui/auth/fragments/LoginFragment.kt
@@ -1,16 +1,37 @@
 package com.example.socialnetworkapp.ui.auth.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.socialnetworkapp.R
+import com.example.socialnetworkapp.other.EventObserver
+import com.example.socialnetworkapp.ui.auth.AuthViewModel
+import com.example.socialnetworkapp.ui.main.MainActivity
+import com.example.socialnetworkapp.ui.snackbar
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_login.*
 
+@AndroidEntryPoint
 class LoginFragment : Fragment(R.layout.fragment_login){
+
+    private lateinit var viewModel: AuthViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity()).get(AuthViewModel::class.java)
+        subscribeToObservers()
+
+        btnLogin.setOnClickListener{
+            viewModel.login(
+                    etEmail.text.toString(),
+                    etPassword.text.toString()
+            )
+        }
 
         tvRegisterNewAccount.setOnClickListener {
             if(findNavController().previousBackStackEntry != null){
@@ -20,6 +41,22 @@ class LoginFragment : Fragment(R.layout.fragment_login){
                         LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
                 )
         }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.loginStatus.observe(viewLifecycleOwner,EventObserver(
+                onError = {
+                    loginProgressBar.isVisible = false
+                    snackbar(it)
+                },
+                onLoading = {loginProgressBar.isVisible = true }
+        ) {
+            loginProgressBar.isVisible = false
+            Intent(requireContext(), MainActivity::class.java).also {
+                startActivity(it)
+                requireActivity().finish()
+            }
+        })
     }
 
 }
