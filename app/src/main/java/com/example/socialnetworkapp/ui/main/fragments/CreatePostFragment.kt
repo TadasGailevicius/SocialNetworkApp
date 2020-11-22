f87bb3aa8055569fff398aafcc7d9016245f069f
Tadas <tadasgailevicius@yahoo.com>
2020-11-22 21:45:22 +0200

    Add CreatePostFragment.kt fragment into \ui\main\fragments

---
+++ app/src/main/java/com/example/socialnetworkapp/ui/main/fragments/CreatePostFragment.kt
@@ -0,0 +1,8 @@
+package com.example.socialnetworkapp.ui.main.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.socialnetworkapp.R
+
+class CreatePostFragment : Fragment(R.layout.fragment_create_post){
+
+}
