08d76556e04801a023594bf969eefa887aacacc1
Tadas <tadasgailevicius@yahoo.com>
2020-11-22 15:52:59 +0200

    Add additional constants into Constants.kt

--- app/src/main/java/com/example/socialnetworkapp/other/Constants.kt
+++ app/src/main/java/com/example/socialnetworkapp/other/Constants.kt
@@ -4,4 +4,8 @@ object Constants {
 
     const val DEFAULT_PROFILE_PICTURE_URL = "https://firebasestorage.googleapis.com/v0/b/socialnetworkapp-bc788.appspot.com/o/img_avatar.png?alt=media&token=dae3b800-f2d6-4110-84bd-e1bd6fae962a"
 
+    const val MIN_USERNAME_LENGHT = 3
+    const val MAX_USERNAME_LENGHT = 8
+    const val MIN_PASSWORD_LENGHT = 8
+
 }
