2f22356cbbfa78f445f55a41bc45d2ba74db3675
Tadas <tadasgailevicius@yahoo.com>
2020-11-22 21:47:49 +0200

    Add SettingsFragment.kt fragment into ui\main\fragments

---
+++ app/src/main/java/com/example/socialnetworkapp/ui/main/fragments/SettingsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.socialnetworkapp.ui.main.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.socialnetworkapp.R
+
+class SettingsFragment : Fragment(R.layout.fragment_create_post){
+
+}
