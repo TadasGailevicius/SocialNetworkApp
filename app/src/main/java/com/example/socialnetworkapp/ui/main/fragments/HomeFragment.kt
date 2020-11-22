6f49abf1ffeb061773a798bdba61b3dc4dfead78
Tadas <tadasgailevicius@yahoo.com>
2020-11-22 21:42:44 +0200

    Add HomeFragment.kt fragment into ui\main\fragments

---
+++ app/src/main/java/com/example/socialnetworkapp/ui/main/fragments/HomeFragment.kt
@@ -0,0 +1,8 @@
+package com.example.socialnetworkapp.ui.main.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.socialnetworkapp.R
+
+class HomeFragment : Fragment(R.layout.fragment_home){
+    
+}
