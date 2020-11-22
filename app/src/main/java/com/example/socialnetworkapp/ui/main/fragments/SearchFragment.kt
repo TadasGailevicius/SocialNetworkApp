9c484e4f188666bffd37196b50a8865b0707ea7f
Tadas <tadasgailevicius@yahoo.com>
2020-11-22 21:44:02 +0200

    Add SearchFragment.kt fragment into ui\main\fragments

--- app/src/main/java/com/example/socialnetworkapp/ui/main/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/socialnetworkapp/ui/main/fragments/SearchFragment.kt
@@ -3,6 +3,6 @@ package com.example.socialnetworkapp.ui.main.fragments
 import androidx.fragment.app.Fragment
 import com.example.socialnetworkapp.R
 
-class HomeFragment : Fragment(R.layout.fragment_home){
+class SearchFragment : Fragment(R.layout.fragment_search){
 
 }
