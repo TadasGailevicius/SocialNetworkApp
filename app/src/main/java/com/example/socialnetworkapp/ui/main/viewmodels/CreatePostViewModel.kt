d1cca463c8cb0e7ad029680f856613424555df2d
Tadas <tadasgailevicius@yahoo.com>
2020-11-24 23:22:36 +0200

    Add CreatePostViewModel.kt ViewModel

---
+++ app/src/main/java/com/example/socialnetworkapp/ui/main/viewmodels/CreatePostViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.socialnetworkapp.ui.main.viewmodels
+
+import android.content.Context
+import android.net.Uri
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.socialnetworkapp.R
+import com.example.socialnetworkapp.other.Event
+import com.example.socialnetworkapp.other.Resource
+import com.example.socialnetworkapp.repositories.MainRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class CreatePostViewModel @ViewModelInject constructor(
+        private val repository: MainRepository,
+        private val applicationContext: Context,
+        private val dispatcher: CoroutineDispatcher = Dispatchers.Main
+) : ViewModel(){
+
+    private val _createPostStatus = MutableLiveData<Event<Resource<Any>>>()
+    private val createPostStatus: LiveData<Event<Resource<Any>>> = _createPostStatus
+
+    fun createPost(imageUri: Uri, text: String){
+        if(text.isEmpty()) {
+            val error = applicationContext.getString(R.string.error_input_empty)
+            _createPostStatus.postValue(Event(Resource.Error(error)))
+        } else {
+            _createPostStatus.postValue(Event(Resource.Loading()))
+            viewModelScope.launch(dispatcher) {
+                val result = repository.createPost(imageUri,text)
+                _createPostStatus.postValue(Event(result))
+
+            }
+        }
+    }
+
+}
