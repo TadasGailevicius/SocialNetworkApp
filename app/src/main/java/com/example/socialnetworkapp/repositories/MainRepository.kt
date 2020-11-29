6024879b0de3dc617f30670ec5e329100ccfe54e
Tadas <tadasgailevicius@yahoo.com>
2020-11-29 16:21:54 +0200

    Update MainRepository.kt interface

--- app/src/main/java/com/example/socialnetworkapp/repositories/MainRepository.kt
+++ app/src/main/java/com/example/socialnetworkapp/repositories/MainRepository.kt
@@ -1,9 +1,14 @@
 package com.example.socialnetworkapp.repositories
 
 import android.net.Uri
+import com.example.socialnetworkapp.data.entities.User
 import com.example.socialnetworkapp.other.Resource
 
 interface MainRepository {
 
-    suspend fun  createPost(imageUri: Uri, text: String): Resource<Any>
+    suspend fun createPost(imageUri: Uri, text: String): Resource<Any>
+
+    suspend fun getUsers(uids: List<String>): Resource<List<User>>
+
+    suspend fun getUser(uid: String): Resource<User>
 }
