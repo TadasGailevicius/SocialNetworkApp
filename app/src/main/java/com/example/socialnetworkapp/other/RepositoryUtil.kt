607e21ba8968117be73649f413c177f9871ac446
Tadas <tadasgailevicius@yahoo.com>
2020-12-28 21:58:34 +0200

    Fix RepositoryUtil.kt

--- app/src/main/java/com/example/socialnetworkapp/other/RepositoryUtil.kt
+++ app/src/main/java/com/example/socialnetworkapp/other/RepositoryUtil.kt
@@ -1,11 +1,12 @@
 package com.example.socialnetworkapp.other
 
+import com.google.firebase.firestore.FirebaseFirestoreException
 import java.lang.Exception
 
 inline fun <T> safeCall(action: () -> Resource<T>) : Resource<T> {
     return try {
         action()
     } catch (e: Exception){
-        Resource.Error(e.message ?: "An unknown error occured")
+        Resource.Error(e.localizedMessage ?: "An unknown error occured")
     }
 }
